8b91d5a5a5a2486d66c20c62858621ad2eafcdfd
developersaifx <saifl6941@gmail.com>
2023-03-27 16:02:39 +0530

    Adding Font color

--- dom.js
+++ dom.js
@@ -1,2 +1,10 @@
 var headerTitle = document.getElementById('header-title');
-headerTitle.style.borderBottom = 'solid 3px #000';
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'DOM 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor ='green';
