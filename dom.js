db072d27e0ad5aa26c67ad356b30dd1f05427927
developersaifx <saifl6941@gmail.com>
2023-03-30 14:58:42 +0530

    3rd element modified

--- dom.js
+++ dom.js
@@ -1,10 +1,28 @@
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+//console.log(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent = 'Hello';
+//console.log(document.form);
+//console.log(document.links);
+//console.log(document.images);
 
-var items = document.getElementByClassName('list-group-item');
+//console.log(document.getElementById);
+
+
+
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('list-group-item');
 console.log(items);
-console.log(items[1]);
-items[1].textContent = 'DOM 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor ='green';
+console.log(items[2]);
+items[2].textContent = 'DOM 3';
+items[2].style.fontWeight = 'bold';
+items[2].style.backgroundColor ='green';
