92e6b08d5ac11dc6d1e7295557e323d0d9eabba4
developersaifx <saifl6941@gmail.com>
2023-04-03 12:40:14 +0530

    new li element

--- dom.js
+++ dom.js
@@ -20,9 +20,22 @@
 //var header = document.getElementById('main-header');
 //header.style.borderBottom = 'solid 3px #000';
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].textContent = 'DOM 3';
-items[2].style.fontWeight = 'bold';
-items[2].style.backgroundColor ='green';
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[2]);
+//items[2].textContent = 'DOM 3';
+//items[2].style.fontWeight = 'bold';
+//items[2].style.backgroundColor ='green';
+
+//GetELEMENTSBYTAGNAME
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li);
+li[1].textContent ='Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+
+for(var i=0; i < li.length; i++){
+    li[i].style.backgroundColor = '#f4f4f4';
+}
