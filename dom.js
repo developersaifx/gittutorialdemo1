3d284d4e43ab7035cede60089ad6555f79d6e4d9
developersaifx <saifl6941@gmail.com>
2023-04-05 15:57:23 +0530

    Creating Nodes and Modifying Dom

--- dom.js
+++ dom.js
@@ -60,14 +60,83 @@
 // secondItem.style.color = 'coral';
 
 //QuerySelector All
-var titles = document.querySelectorAll('.title');
-console.log(titles);
-titles[0].textContent = 'Hello';
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i=0; i < odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// for(var i=0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+//Traversing the DOM
+var itemList = document.querySelector('#items');
+//Parent Node
+
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//Parent Element
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+//console.log(itemList.ChildNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// //FirstChild
+// console.log(itemList.firstChild);
+// //firstElementChild
+// console.log(itemList.firstElementchild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+//lastChild
+//console.log(itemList.lastChild);
+//lastElementChild
+//console.log(itemList.lastElementchild);
+//itemList.lastElementChild.textContent = 'Hello 4';
+
+//nextSibling
+// console.log(itemList.nextSibling);
+// //nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+//previousSibling
+// console.log(itemList.previousSibling);
+// //previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+//createElement
+//create a div
+var newDiv = document.createElement('div');
+//Add class
+newDiv.className = 'hello';
+
+//Add id
+newDiv.id ='hello1';
+
+//Add attr
+newDiv.setAttribute('title','Hello Div');
+
+//create text node
+var newDivText = document.createTextNode('Hello world');
+
+//Add text to div
+newDiv.appendChild(newDivText);
+
+var container =document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
