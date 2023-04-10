4957ca0f35fa2582dfc1475c99e4ebe0ae252eae
developersaifx <saifl6941@gmail.com>
2023-04-10 15:25:31 +0530

    Delete and Edit functionality

--- dom.js
+++ dom.js
@@ -117,26 +117,93 @@ var itemList = document.querySelector('#items');
 
 //createElement
 //create a div
-var newDiv = document.createElement('div');
-//Add class
-newDiv.className = 'hello';
+// var newDiv = document.createElement('div');
+// //Add class
+// newDiv.className = 'hello';
 
-//Add id
-newDiv.id ='hello1';
+// //Add id
+// newDiv.id ='hello1';
 
-//Add attr
-newDiv.setAttribute('title','Hello Div');
+// //Add attr
+// newDiv.setAttribute('title','Hello Div');
 
-//create text node
-var newDivText = document.createTextNode('Hello world');
+// //create text node
+// var newDivText = document.createTextNode('Hello world');
 
-//Add text to div
-newDiv.appendChild(newDivText);
+// //Add text to div
+// newDiv.appendChild(newDivText);
 
-var container =document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// var container =document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
-console.log(newDiv);
+// console.log(newDiv);
 
-newDiv.style.fontSize = '30px';
-container.insertBefore(newDiv, h1);
+// newDiv.style.fontSize = '30px';
+// container.insertBefore(newDiv, h1);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
